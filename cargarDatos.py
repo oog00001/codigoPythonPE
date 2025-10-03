da89e6c000ba901353ddfe2a3959babaf50c8b81
Oscar Ortega <oog00001@red.ujaen.es>
2025-10-03 10:53:42 +0200

    10:53 3/10/2025

--- cargarDatos.py
+++ cargarDatos.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import pruebasConAPI
-from typing import List
 
 # las peliculas son id,title,genre_ids,poster_path
 
@@ -11,10 +10,6 @@ def cargar_peliculas():
 def cargar_usuarios():
     data = pd.read_csv("ml-1m/ratings.dat", sep="::",
                     names=["user", "item", "label", "timestamp"])
-    movie = cargar_peliculas()
-    moviesRated: List[pruebasConAPI.MovieOnDB]
-    userProfile = pruebasConAPI.getUserProfile(moviesRated)
-    print(userProfile)
 
 def cargar_ratings():
     data = pd.read_csv("ml-1m/ratings.dat", sep="::",
